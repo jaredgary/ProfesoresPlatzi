87f44b9b495e83b5aab9396e4f430fbc96efeeb3
Gary Gonzalez <garygonzalezzepeda@gmail.com>
2018-04-01 13:45:12 -0600

    first commit

---
+++ src/main/java/com/platzi/profesoresplatzi/configuration/DataBaseConfiguration.java
@@ -0,0 +1,57 @@
+package com.platzi.profesoresplatzi.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DataBaseConfiguration {
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory(){
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource());
+		sessionFactoryBean.setPackagesToScan("com.platzi.profesoresplatzi.model");
+		sessionFactoryBean.setHibernateProperties(hibernateProperties());
+		return sessionFactoryBean;
+	}
+	
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://us-cdbr-iron-east-03.cleardb.net/heroku_7e6331b00d902dd");
+		dataSource.setUsername("bd4826c410bb3f");
+		dataSource.setPassword("e34b2d5d");
+		
+		return dataSource;
+	}
+	
+	
+	public Properties hibernateProperties(){
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		properties.put("show_sql", "true");
+		
+		return properties;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+		hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
+		return hibernateTransactionManager;
+	}
+	
+}
